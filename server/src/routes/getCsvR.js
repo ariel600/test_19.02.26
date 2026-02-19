3d142b63d67108d4617632aa9ad0a075de0d62ad
Ariel_Naiman <a0527633600@gmail.com>
2026-02-19 09:53:21 +0200

    commit

--- server/src/routes/getCsvR.js
+++ server/src/routes/getCsvR.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { getCsvC } from "../controllers/getCsvC.js";
 
 const router = express.Router();
 
-router.get("/", get)
+router.get("/", getCsvC)
 
 export default router
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import getCsvR from "./routes/getCsvR.js";
 import scoreR from "./routes/scoreR.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => res.json({ runing: true }))
 app.use("/getCsv", getCsvR);
 app.use("/score", scoreR);
 
